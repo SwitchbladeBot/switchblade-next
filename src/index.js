43ced4ccbc8e1a82b8e12d07f811d7212bd9ac91
pedrofracassi <pedroluizfracassi@gmail.com>
2020-04-09 22:39:32 +0000

    Add clustering and shards

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const { Switchblade } = require('./structures/base')
 
-const client = new Switchblade(process.env.DISCORD_TOKEN, {
-    firstShardID: process.env.SHARD_ID || 0,
-    maxShards: 1
-}, {
-    prefix: process.env.PREFIX || 'n!',
+const CLUSTER_ID = process.env.INDEX_CLUSTERS_FROM_ONE ? parseInt(process.env.CLUSTER_ID) - 1 : parseInt(process.env.CLUSTER_ID)
+const firstShardID = CLUSTER_ID * parseInt(process.env.SHARDS_PER_CLUSTER)
+const lastShardID = ((CLUSTER_ID + 1) * parseInt(process.env.SHARDS_PER_CLUSTER)) - 1
+const maxShards = parseInt(process.env.SHARDS_PER_CLUSTER) * parseInt(process.env.MAX_CLUSTERS)
 
+const client = new Switchblade(process.env.DISCORD_TOKEN, { firstShardID, lastShardID, maxShards }, {
+    prefix: process.env.PREFIX || 'n!' 
 })
+
+client.logger.info(`Starting. First Shard: ${firstShardID}; Last Shard: ${lastShardID}`, { label: `Cluster ${process.env.CLUSTER_ID}` })
